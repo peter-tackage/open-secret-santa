5924e5ac46f32bb4ac24b31eb88948dd5fbe1904
peter-tackage <peter.tackage@gmail.com>
2013-05-26 22:16:56 +0200

    Implemented bespoke queries

--- open-secret-santa-app/src/main/java/com/moac/android/opensecretsanta/database/DatabaseManager.java
+++ open-secret-santa-app/src/main/java/com/moac/android/opensecretsanta/database/DatabaseManager.java
@@ -1,6 +1,7 @@
 package com.moac.android.opensecretsanta.database;
 
 import android.database.SQLException;
+import com.j256.ormlite.stmt.DeleteBuilder;
 import com.moac.android.opensecretsanta.types.*;
 
 import java.util.List;
@@ -42,7 +43,7 @@ public class DatabaseManager {
     }
 
     /**
-     * Groups
+     * Bespoke queries
      */
 
     public List<Group> queryAllGroupsWhereReady() {
@@ -56,36 +57,75 @@ public class DatabaseManager {
     }
 
     public DrawResult queryLatestDrawResultForGroup(long groupId) {
-      // TODO Implement
-        return  null;
+        try {
+            return mDbHelper.getDaoEx(DrawResult.class).queryBuilder()
+              .orderBy(DrawResult.Columns.DRAW_DATE_COLUMN, false)
+              .where().eq(DrawResult.Columns.GROUP_ID_COLUMN, groupId)
+              .queryForFirst();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
     public List<Restriction> queryAllRestrictionsForMemberId(long memberId) {
-        // TODO Implement
-        return  null;
+        try {
+            return mDbHelper.getDaoEx(Restriction.class).queryBuilder()
+              .where().eq(Restriction.Columns.MEMBER_ID_COLUMN, memberId)
+              .query();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
-    public List<DrawResultEntry> queryAllDrawResultEntriesForDrawId(long id) {
-        // TODO Implement
-        return  null;
+    public List<DrawResultEntry> queryAllDrawResultEntriesForDrawId(long drawResultId) {
+        try {
+            return mDbHelper.getDaoEx(DrawResultEntry.class).queryBuilder()
+              .where().eq(DrawResultEntry.Columns.DRAW_RESULT_ID_COLUMN, drawResultId)
+              .query();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
     public List<DrawResult> queryAllDrawResultsForGroup(long groupId) {
-        // TODO Implement
-        return  null;
+        try {
+            return mDbHelper.getDaoEx(DrawResult.class).queryBuilder()
+              .where().eq(DrawResult.Columns.GROUP_ID_COLUMN, groupId)
+              .query();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
     public List<Member> queryAllMembersForGroup(long groupId) {
-        // TODO Implement
-        return  null;
+        try {
+            return mDbHelper.getDaoEx(Member.class).queryBuilder()
+              .where().eq(Member.Columns.GROUP_ID_COLUMN, groupId)
+              .query();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
     public Member queryMemberWithNameForGroup(long groupId, String name) {
-        // TODO Implement
-        return  null;
+        try {
+            return mDbHelper.getDaoEx(Member.class).queryBuilder()
+              .where().eq(Member.Columns.GROUP_ID_COLUMN, groupId)
+              .and()
+              .eq(Member.Columns.NAME_COLUMN, name)
+              .queryForFirst();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 
     public void deleteAllRestrictionsForMember(long memberId) {
-        // TODO Implement
+        try {
+            DeleteBuilder<Restriction, Long> deleteBuilder = mDbHelper.getDaoEx(Restriction.class).deleteBuilder();
+            deleteBuilder.where().eq(Restriction.Columns.MEMBER_ID_COLUMN, memberId);
+            deleteBuilder.delete();
+        } catch(java.sql.SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
     }
 }
