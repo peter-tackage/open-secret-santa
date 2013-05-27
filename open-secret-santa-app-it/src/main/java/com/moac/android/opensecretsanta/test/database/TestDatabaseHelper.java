a3131a17e0a10ed717240ec9c1c4cd2bd0ce5b5b
peter-tackage <peter.tackage@gmail.com>
2013-05-27 12:20:03 +0200

    Add skeletons database tests

---
+++ open-secret-santa-app-it/src/main/java/com/moac/android/opensecretsanta/test/database/TestDatabaseHelper.java
@@ -0,0 +1,13 @@
+package com.moac.android.opensecretsanta.test.database;
+
+import android.content.Context;
+import com.moac.android.opensecretsanta.database.DatabaseHelper;
+
+public class TestDatabaseHelper extends DatabaseHelper {
+
+    private static final String TEST_DATBASE_NAME = "testopensecretsanta.db";
+
+    protected TestDatabaseHelper(Context context) {
+        super(context, TEST_DATBASE_NAME);
+    }
+}
