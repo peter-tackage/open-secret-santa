3513da70a9e6a09c04dc29cca1c3706ebe048abf
peter-tackage <peter.tackage@gmail.com>
2013-06-13 13:38:11 +0200

    Cleanup unused and unnecessary ViewHolder references.

--- open-secret-santa-app/src/main/java/com/moac/android/opensecretsanta/activity/MemberListAdapter.java
+++ open-secret-santa-app/src/main/java/com/moac/android/opensecretsanta/activity/MemberListAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.moac.android.opensecretsanta.R;
@@ -15,93 +14,58 @@ import java.util.List;
 
 public class MemberListAdapter extends ArrayAdapter<MemberRowDetails> {
 
-    private final String TAG = "MemberListAdapter";
+    private final String TAG = MemberListAdapter.class.getSimpleName();
 
-    private int resource;
-    private OnClickListener mRestrictClickListener;
-    private OnClickListener mDeleteClickListener;
+    private int mResource;
 
-    public MemberListAdapter(Context context, int _resource,
-                             List<MemberRowDetails> _draws, View.OnClickListener _restrictListener, View.OnClickListener _deleteListener) {
-        super(context, _resource, _draws);
-        resource = _resource;
-        mRestrictClickListener = _restrictListener;
-        mDeleteClickListener = _deleteListener;
+    public MemberListAdapter(Context _context, int _resource,
+                             List<MemberRowDetails> _members) {
+        super(_context, _resource, _members);
+        mResource = _resource;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-//		Log.v(TAG, "getView() - start");
-//		Log.v(TAG, "getView() - position: " + position);
+    public View getView(int _position, View _convertView, ViewGroup _parent) {
 
-        View v = convertView;
+        View v = _convertView;
         ViewHolder holder;
         LinearLayout newView;
 
         if(v == null) {
             newView = new LinearLayout(getContext());
-            String inflator = Context.LAYOUT_INFLATER_SERVICE;
-            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflator);
-            v = vi.inflate(resource, newView, true);
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            _convertView = inflater.inflate(mResource, newView, true);
 
-            // the ViewHolder keeps references to avoid making unnecessary calls to findViewById
             holder = new ViewHolder();
-            holder.memberName = (TextView) newView.findViewById(R.id.memberName);
-            holder.restrictionsCount = (TextView) newView.findViewById(R.id.restrictionCountTextView);
+            holder.mMemberNameView = (TextView) newView.findViewById(R.id.member_name_textview);
+            holder.mContactModeView = (TextView) newView.findViewById(R.id.contact_mode_textview);
+            holder.mRestrictionsView = (TextView) newView.findViewById(R.id.restriction_count_textview);
 
-            holder.restrictMembersButton = (ImageView) newView.findViewById(R.id.restrictMembersImage);
-            holder.deleteButton = (ImageView) newView.findViewById(R.id.removeMemberButton);
-            holder.contactMode = (TextView) newView.findViewById(R.id.contactMode);
-            holder.memberEntryLayout = (LinearLayout) newView.findViewById(R.id.memberEntryLayout);
-
-            v.setTag(holder);
+            _convertView.setTag(holder);
         } else {
-            // view already exists, get the holder instance from the view
+            // View already exists, retrieve the holder instance from the View
             holder = (ViewHolder) v.getTag();
         }
 
-        // Get the individual parts of the draw_row and populate.
-        MemberRowDetails details = getItem(position);
+        MemberRowDetails details = getItem(_position);
+        holder.mMemberNameView.setText(details.mMemberName);
+        holder.mContactModeView.setText(details.mContactDetail);
 
-        holder.memberName.setText(details.memberName);
-        holder.contactMode.setText(details.contactDetail);
-
-        String count = (details.restrictionCount > 0) ? String.valueOf(details.restrictionCount) : "";
-        holder.restrictionsCount.setText(count);
-        holder.restrictionsCount.setVisibility((details.restrictionCount == 0) ? View.GONE : View.VISIBLE);
-
-        // the delete button should be clickable to trigger a delete of the member
-        holder.deleteButton.setTag(new DeleteViewTag(details.memberId, details.memberName));
-        holder.deleteButton.setOnClickListener(mDeleteClickListener);
-
-        // the open restrictions button too...
-        holder.restrictMembersButton.setTag(details);
-        holder.restrictMembersButton.setOnClickListener(mRestrictClickListener);
+        if(details.mRestrictionCount > 0) {
+            holder.mRestrictionsView.setText(String.valueOf(details.mRestrictionCount));
+            holder.mRestrictionsView.setVisibility(View.VISIBLE);
+        } else {
+            holder.mRestrictionsView.setVisibility(View.GONE);
+        }
 
         v.setTag(holder);
         return v;
     }
 
-    class ViewHolder {
-        ImageView deleteButton;
-        TextView memberName;
-
-        TextView restrictionsCount;
-        ImageView restrictMembersButton;
-
-        TextView contactMode;
-        ImageView contactModeImage;
-
-        LinearLayout memberEntryLayout;
-    }
-
-    class DeleteViewTag {
-        long memberId;
-        String memberName;
-
-        public DeleteViewTag(long _memberId, String _memberName) {
-            this.memberId = _memberId;
-            this.memberName = _memberName;
-        }
+    // the ViewHolder keeps references to avoid making unnecessary calls to findViewById
+    private class ViewHolder {
+        TextView mMemberNameView;
+        TextView mRestrictionsView;
+        TextView mContactModeView;
     }
 }
