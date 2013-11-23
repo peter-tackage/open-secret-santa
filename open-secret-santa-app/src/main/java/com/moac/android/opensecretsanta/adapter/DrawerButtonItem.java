934a5b15558e32e3702c6318600fa8c1326d1bea
peter.tackage <peter.tackage@tunedin.de>
2013-11-23 15:17:27 +0100

    Add side draw layout
    Missing callbacks and handling of group list changes

---
+++ open-secret-santa-app/src/main/java/com/moac/android/opensecretsanta/adapter/DrawerButtonItem.java
@@ -0,0 +1,47 @@
+package com.moac.android.opensecretsanta.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.moac.android.opensecretsanta.R;
+
+public class DrawerButtonItem implements DrawerListAdapter.Item {
+
+    private final Drawable mDrawable;
+    private final String mText;
+
+    public DrawerButtonItem(Drawable drawable, String text) {
+        mDrawable = drawable;
+        mText = text;
+    }
+
+    @Override
+    public int getItemType() {
+        return DrawerListAdapter.ItemType.BUTTON.ordinal();
+    }
+
+    @Override
+    public long getItemId() {
+        return -1;
+    }
+
+    @Override
+    public View getView(Context _context, View _convertView, ViewGroup _parent) {
+        View view = _convertView;
+        TextView textView;
+        if(view == null) {
+            view = LayoutInflater.from(_context).inflate(R.layout.drawer_button_view, _parent, false);
+            textView = (TextView) view.findViewById(R.id.tv_button_label);
+            view.setTag(R.id.tv_button_label, textView);
+        } else {
+            textView = (TextView) view.getTag(R.id.tv_button_label);
+        }
+
+        textView.setText(mText);
+        textView.setCompoundDrawablesWithIntrinsicBounds(mDrawable, null, null, null);
+        return view;
+    }
+}
